7cd36bc726e47965da7fba38c16c524b7106af3c
indypeters2006 <46998095+indypeters2006@users.noreply.github.com>
2021-11-24 23:15:04 +0100

    WUP
    WUPIDIEWUPWUP

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../../client/src/html/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
