a381b82a3e7bf187a8c5d08abaab58a1d9a010be
k <k@k.nl>
2021-12-03 16:36:33 +0100

    fixed not starting bug

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,7 @@ module.exports = class App {
             enabled: true,
             writeToFile: 'xmrlog.txt',
             level: 'debug',
-            writeToConsole: false
+            writeToConsole: true
         }
     };
 
